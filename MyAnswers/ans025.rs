fe442445b0e05c2eabffc66d5af6736121f8aea0
kwsm114514 <88318477+kwsm114514@users.noreply.github.com>
2021-09-23 14:15:49 +0900

    Create ans025.rs

---
+++ MyAnswers/ans025.rs
@@ -0,0 +1,41 @@
+use proconio::input;
+
+const PRIMES: [i64; 4] = [2, 3, 5, 7];
+
+// f(x) = (x の各位の数字の積)
+// m - f(m) = b を満たす　1 <= m <= n の数
+fn main() {
+    input!{n: i64, b: i64}
+
+    // dfs(初めの数1, PRIMESのindex, 終わりの数n, b) = mの数
+    let ans = dfs(1, 0, n, b) + if check(0, n, b) { 1 } else { 0 };
+    println!("{}", ans);
+}
+
+// m - f(m) = b を満たすか?
+fn check(x: i64, lim: i64, b: i64) -> bool {
+    let mut f = x + b;
+    if f > lim { 
+        return false; 
+    }
+    let mut c = 1;
+    while f > 0 {
+        c *= f % 10;
+        f /= 10;
+    }
+    c == x
+}
+
+// dfs(初めの数1, PRIMESのindex, 終わりの数n, b) = mの数
+fn dfs(mut cur: i64, idx: usize, lim: i64, b: i64) -> usize {
+    // 境界の設定
+    if idx == 4 {
+        return if check(cur, lim, b) { 1 } else { 0 }
+    }
+    let mut res = 0;
+    while cur <= lim {
+        res += dfs(cur, idx + 1, lim, b);
+        cur *= PRIMES[idx];
+    }
+    res
+}
